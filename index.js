bb834cec0ffb0055fab1db81bd0ba7249e244e7e
IsaacK47 <isaackayombo41@gmail.com>
2024-09-05 12:50:22 +0200

    button style and function added

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-
 // VARIABLE SECTION
 let firstCard = 10
 let secondCard = 11
@@ -14,14 +8,17 @@ let message = ""
 
 
 // CONDITIONAL SECTION
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Yay! You've got Blackjack! 🤩"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Yay! You've got Blackjack! 🤩"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    
+    console.log(message)
 }
 
-console.log(message)
