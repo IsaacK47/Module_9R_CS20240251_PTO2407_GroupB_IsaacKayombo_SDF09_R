3f236f6ac7a8610c7afe2b6bc03255eb4991409f
IsaacK47 <isaackayombo41@gmail.com>
2024-09-06 21:55:47 +0200

    Object section added

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+// OBJECT SECTION
+let player = {
+    name: "Isaac",
+    chips: 145
+}
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 
 // CONDITIONAL SECTION
 
